ec0d227538679421c757c43091428f796d44e03c
Erik <erikfilias@gmail.com>
2020-09-14 10:14:58 +0200

    update file path

--- src/openTEPES/openTEPES.py
+++ src/openTEPES/openTEPES.py
@@ -109,9 +109,6 @@ InputData(CaseName, mTEPES)
 
 ModelFormulation(mTEPES)
 
-# CWD = os.getcwd()
-# Data_Path = CWD + '\\' + CaseName
-# os.chdir(Data_Path)
 mTEPES.write('openTEPES_'+CaseName+'.lp',
              io_options={'symbolic_solver_labels': True})  # create lp-format file
 
