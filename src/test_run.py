7a57487f7b102bee48850a19fa322b59e68633db
Erik <erikfilias@gmail.com>
2020-11-11 14:00:28 +0100

    Update test_run.py

--- src/test_run.py
+++ src/test_run.py
@@ -16,7 +16,7 @@ CaseName = '9n'
 #%% model declaration
 mTEPES = ConcreteModel('Open Generation and Transmission Operation and Expansion Planning Model with RES and ESS (openTEPES) - Version 1.7.15 - September 4, 2020')
 
-InputData(CaseName, mTEPES)
+# InputData(CaseName, mTEPES)
 
 TotalTime = time.time() - StartTime
 print('Total time                            ... ', round(TotalTime), 's')
