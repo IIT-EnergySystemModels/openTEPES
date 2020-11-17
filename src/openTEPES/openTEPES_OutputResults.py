440882c576513ef62dc626a0b13f08173f11e401
Erik Alvarez <ealvarezq@comillas.edu>
2020-11-17 16:52:59 +0100

    openTEPES

--- src/openTEPES/openTEPES_OutputResults.py
+++ src/openTEPES/openTEPES_OutputResults.py
@@ -91,18 +91,14 @@ def OutputResults(CaseName, mTEPES):
         OutputResults = pd.Series(data=[OutputResults[sc,p,n].filter(mTEPES.t2g[gt]).sum()    for sc,p,n,gt in mTEPES.sc*mTEPES.p*mTEPES.n*mTEPES.gt], index=pd.MultiIndex.from_tuples(list(mTEPES.sc*mTEPES.p*mTEPES.n*mTEPES.gt)))
         OutputResults.to_frame(name='GWh').reset_index().pivot_table(index=['level_0','level_1','level_2'], columns='level_3', values='GWh').rename_axis(['Scenario','Period','LoadLevel'], axis=0).rename_axis([None], axis=1).to_csv(CaseName+'/oT_Result_ESSTechnologyEnergy_'+CaseName+'.csv', sep=',')
 
-        ESSTechnologyEnergy = pd.Series(data=[0]*len(mTEPES.gt), index=list(mTEPES.gt))
-        for gt in mTEPES.gt:
-            ESSTechnologyEnergy[gt] = -OutputResults.loc[:,:,:,gt].sum()
+        OutputResults *= -1
+        ESSTechnologyEnergy = OutputResults.to_frame(name='GWh')
 
-        fig, fg = plt.subplots()
-        def func(percentage, ESSTechnologyEnergy):
-            absolute = int(percentage/100.*ESSTechnologyEnergy.sum())
-            return '{:.1f}%\n({:d} GWh)'.format(percentage, absolute)
-        wedges, texts, autotexts = plt.pie(ESSTechnologyEnergy, labels=list(mTEPES.gt), autopct=lambda percentage: func(percentage, ESSTechnologyEnergy), shadow=True, startangle=90, textprops=dict(color='w'))
-        plt.legend(wedges, list(mTEPES.gt), title='Energy Consumption', loc='center left', bbox_to_anchor=(1, 0, 0.5, 1))
-        plt.setp(autotexts, size=8, weight='bold')
-        plt.axis('equal')
+        ESSTechnologyEnergy['GWh'] = (ESSTechnologyEnergy['GWh'] / ESSTechnologyEnergy['GWh'].sum()) * 100
+        ESSTechnologyEnergy.reset_index(level=3, inplace=True)
+        ESSTechnologyEnergy.groupby(['level_3']).sum().plot(kind='pie', subplots=True, shadow=False, startangle=90,
+                                        figsize=(15, 10), autopct='%1.1f%%')
+        plt.legend(list(mTEPES.gt), title='Energy Consumption', loc='center left', bbox_to_anchor=(1, 0, 0.5, 1))
         # plt.show()
         plt.savefig(CaseName+'/oT_Plot_ESSTechnologyEnergy_'+CaseName+'.png', bbox_inches=None, dpi=600)
 
@@ -155,20 +151,21 @@ def OutputResults(CaseName, mTEPES):
     # takes a long time
     OutputResults = pd.Series(data=[mTEPES.vTotalOutput[sc,p,n,g]()*mTEPES.pDuration[n] for sc,p,n,g in mTEPES.sc*mTEPES.p*mTEPES.n*mTEPES.g ], index=pd.MultiIndex.from_tuples(list(mTEPES.sc*mTEPES.p*mTEPES.n*mTEPES.g )))
     OutputResults = pd.Series(data=[OutputResults[sc,p,n].filter(mTEPES.t2g[gt]).sum() for sc,p,n,gt in mTEPES.sc*mTEPES.p*mTEPES.n*mTEPES.gt], index=pd.MultiIndex.from_tuples(list(mTEPES.sc*mTEPES.p*mTEPES.n*mTEPES.gt)))
-    OutputResults.to_frame(name='GWh').reset_index().pivot_table(index=['level_0','level_1','level_2'], columns='level_3', values='GWh').rename_axis(['Scenario','Period','LoadLevel'], axis=0).rename_axis([None], axis=1).to_csv(CaseName+'/oT_Result_TechnologyEnergy_'+CaseName+'.csv', sep=',')
 
-    TechnologyEnergy = OutputResults.to_frame(name='GWh').reset_index().pivot_table(index=['level_0','level_1','level_2'], columns='level_3', values='GWh').rename_axis(['Scenario','Period','LoadLevel'], axis=0).rename_axis([None], axis=1).sum()
+    TechnologyEnergy = OutputResults.to_frame(name='GWh')
 
-    fig, fg = plt.subplots()
-    def func(percentage, TechnologyEnergy):
-        absolute = int(percentage/100.*TechnologyEnergy.sum())
-        return '{:.1f}%\n({:d} GWh)'.format(percentage, absolute)
-    wedges, texts, autotexts = plt.pie(TechnologyEnergy, labels=list(mTEPES.gt), autopct=lambda percentage: func(percentage, TechnologyEnergy), shadow=True, startangle=90, textprops=dict(color='w'))
-    plt.legend(wedges, list(mTEPES.gt), title='Energy Generation', loc='center left', bbox_to_anchor=(1, 0, 0.5, 1))
-    plt.setp(autotexts, size=8, weight='bold')
-    plt.axis('equal')
+    TechnologyEnergy['GWh'] = (TechnologyEnergy['GWh'] / TechnologyEnergy['GWh'].sum()) * 100
+    TechnologyEnergy.reset_index(level=3, inplace=True)
+
+    TechnologyEnergy.groupby(['level_3']).sum().plot(kind='pie', subplots=True, shadow=False, startangle=-40,
+                                                        figsize=(15, 10), autopct='%1.1f%%', pctdistance=0.85)
+    plt.legend(list(mTEPES.gt), title='Energy Generation', loc='center left', bbox_to_anchor=(1, 0, 0.5, 1))
+
+    centre_circle = plt.Circle((0, 0), 0.70, fc='white')
+    fig = plt.gcf()
+    fig.gca().add_artist(centre_circle)
     # plt.show()
-    plt.savefig(CaseName+'/oT_Plot_TechnologyEnergy_'+CaseName+'.png', bbox_inches=None, dpi=600)
+    plt.savefig(CaseName + '/oT_Plot_TechnologyEnergy_' + CaseName + '.png', bbox_inches=None, dpi=600)
 
     # OutputResults = pd.Series(data=[mTEPES.vTotalOutput[sc,p,n,g]()*mTEPES.pDuration[n] for sc,p,n,g in mTEPES.sc*mTEPES.p*mTEPES.n*mTEPES.g ], index=pd.MultiIndex.from_tuples(list(mTEPES.sc*mTEPES.p*mTEPES.n*mTEPES.g )))
     # OutputResults = pd.Series(data=[OutputResults[sc,p,n].filter(mTEPES.t2g[gt]).sum()/mTEPES.pRatedMaxPower.filter(mTEPES.t2g[gt]).sum() for sc,p,n,gt in mTEPES.sc*mTEPES.p*mTEPES.n*mTEPES.gt], index=pd.MultiIndex.from_tuples(list(mTEPES.sc*mTEPES.p*mTEPES.n*mTEPES.gt)))
@@ -310,9 +307,7 @@ def OutputResults(CaseName, mTEPES):
     OutputResults.to_frame(name='MEUR').reset_index().pivot_table(index=['level_0','level_1','level_2'], columns='level_4', values='MEUR').rename_axis(['Scenario','Period','LoadLevel'], axis=0).rename_axis([None], axis=1).to_csv(CaseName+'/oT_Result_GenerationPowerRevenue_'   +CaseName+'.csv', sep=',')
 
     #%% plotting the network in a map
-    import geopandas
     import cartopy.crs as ccrs
-    from   cartopy.io import shapereader
     import cartopy.io.img_tiles as cimgt
     import cartopy.feature as cfeature
     from matplotlib.transforms import offset_copy
@@ -326,7 +321,6 @@ def OutputResults(CaseName, mTEPES):
     ax = fig.add_subplot(1, 1, 1, projection=stamen_terrain.crs)
 
     # Limit the extent of the map to a small longitude/latitude range.
-    # ax.set_extent([-22, -15, 63, 65], crs=ccrs.Geodetic())
     ax.set_extent((min(mTEPES.pNodeLon.values()) - 2, max(mTEPES.pNodeLon.values()) + 2,
                    min(mTEPES.pNodeLat.values()) - 2, max(mTEPES.pNodeLat.values()) + 2), crs=ccrs.Geodetic())
 
@@ -342,76 +336,10 @@ def OutputResults(CaseName, mTEPES):
     geodetic_transform = ccrs.Geodetic()._as_mpl_transform(ax)
     text_transform = offset_copy(geodetic_transform, units='dots', x=-10)
 
-    # # take data from http://www.naturalearthdata.com/
-    # df = geopandas.read_file(shapereader.natural_earth(resolution='10m', category='cultural', name='admin_0_countries'))
-    # polyPE = df.loc[df['ADMIN'] == 'Peru']['geometry'].values[0]
-    # polyAT = df.loc[df['ADMIN'] == 'Austria'       ]['geometry'].values[0]
-    # polyBE = df.loc[df['ADMIN'] == 'Belgium'       ]['geometry'].values[0]
-    # polyBG = df.loc[df['ADMIN'] == 'Bulgaria'      ]['geometry'].values[0]
-    # polyCH = df.loc[df['ADMIN'] == 'Croatia'       ]['geometry'].values[0]
-    # polyCY = df.loc[df['ADMIN'] == 'Cyprus'        ]['geometry'].values[0]
-    # polyCZ = df.loc[df['ADMIN'] == 'Czechia'       ]['geometry'].values[0]
-    # polyDK = df.loc[df['ADMIN'] == 'Denmark'       ]['geometry'].values[0]
-    # polyEE = df.loc[df['ADMIN'] == 'Estonia'       ]['geometry'].values[0]
-    # polyFI = df.loc[df['ADMIN'] == 'Finland'       ]['geometry'].values[0]
-    # polyFR = df.loc[df['ADMIN'] == 'France'        ]['geometry'].values[0]
-    # polyDE = df.loc[df['ADMIN'] == 'Germany'       ]['geometry'].values[0]
-    # polyEL = df.loc[df['ADMIN'] == 'Greece'        ]['geometry'].values[0]
-    # polyHU = df.loc[df['ADMIN'] == 'Hungary'       ]['geometry'].values[0]
-    # polyIE = df.loc[df['ADMIN'] == 'Ireland'       ]['geometry'].values[0]
-    # polyIT = df.loc[df['ADMIN'] == 'Italy'         ]['geometry'].values[0]
-    # polyLV = df.loc[df['ADMIN'] == 'Latvia'        ]['geometry'].values[0]
-    # polyLT = df.loc[df['ADMIN'] == 'Lithuania'     ]['geometry'].values[0]
-    # polyLU = df.loc[df['ADMIN'] == 'Luxembourg'    ]['geometry'].values[0]
-    # polyMT = df.loc[df['ADMIN'] == 'Malta'         ]['geometry'].values[0]
-    # polyNL = df.loc[df['ADMIN'] == 'Netherlands'   ]['geometry'].values[0]
-    # polyPL = df.loc[df['ADMIN'] == 'Poland'        ]['geometry'].values[0]
-    # polyPT = df.loc[df['ADMIN'] == 'Portugal'      ]['geometry'].values[0]
-    # polyRO = df.loc[df['ADMIN'] == 'Romania'       ]['geometry'].values[0]
-    # polySK = df.loc[df['ADMIN'] == 'Slovakia'      ]['geometry'].values[0]
-    # polySI = df.loc[df['ADMIN'] == 'Slovenia'      ]['geometry'].values[0]
-    # polyES = df.loc[df['ADMIN'] == 'Spain'         ]['geometry'].values[0]
-    # polySE = df.loc[df['ADMIN'] == 'Sweden'        ]['geometry'].values[0]
-    # polyUK = df.loc[df['ADMIN'] == 'United Kingdom']['geometry'].values[0]
-    #
-    # fg = plt.axes(projection=ccrs.PlateCarree())
-    # fg.stock_img()
-    # fg.set_extent((min(mTEPES.pNodeLon.values())-2, max(mTEPES.pNodeLon.values())+2, min(mTEPES.pNodeLat.values())-2, max(mTEPES.pNodeLat.values())+2), crs=ccrs.PlateCarree())
-    #
-    # # fg.add_geometries([polyAT], crs=ccrs.PlateCarree(), facecolor='C0', edgecolor='0.5')
-    # # fg.add_geometries([polyBE], crs=ccrs.PlateCarree(), facecolor='C1', edgecolor='0.5')
-    # # fg.add_geometries([polyBG], crs=ccrs.PlateCarree(), facecolor='C2', edgecolor='0.5')
-    # # fg.add_geometries([polyCH], crs=ccrs.PlateCarree(), facecolor='C3', edgecolor='0.5')
-    # # fg.add_geometries([polyCY], crs=ccrs.PlateCarree(), facecolor='C4', edgecolor='0.5')
-    # # fg.add_geometries([polyCZ], crs=ccrs.PlateCarree(), facecolor='C5', edgecolor='0.5')
-    # # fg.add_geometries([polyDK], crs=ccrs.PlateCarree(), facecolor='C6', edgecolor='0.5')
-    # # fg.add_geometries([polyEE], crs=ccrs.PlateCarree(), facecolor='C7', edgecolor='0.5')
-    # # fg.add_geometries([polyFI], crs=ccrs.PlateCarree(), facecolor='C8', edgecolor='0.5')
-    # # fg.add_geometries([polyFR], crs=ccrs.PlateCarree(), facecolor='C9', edgecolor='0.5')
-    # # fg.add_geometries([polyDE], crs=ccrs.PlateCarree(), facecolor='C0', edgecolor='0.5')
-    # # fg.add_geometries([polyEL], crs=ccrs.PlateCarree(), facecolor='C1', edgecolor='0.5')
-    # # fg.add_geometries([polyHU], crs=ccrs.PlateCarree(), facecolor='C2', edgecolor='0.5')
-    # # fg.add_geometries([polyIE], crs=ccrs.PlateCarree(), facecolor='C3', edgecolor='0.5')
-    # # fg.add_geometries([polyIT], crs=ccrs.PlateCarree(), facecolor='C4', edgecolor='0.5')
-    # # fg.add_geometries([polyLV], crs=ccrs.PlateCarree(), facecolor='C5', edgecolor='0.5')
-    # # fg.add_geometries([polyLT], crs=ccrs.PlateCarree(), facecolor='C6', edgecolor='0.5')
-    # # fg.add_geometries([polyLU], crs=ccrs.PlateCarree(), facecolor='C7', edgecolor='0.5')
-    # # fg.add_geometries([polyMT], crs=ccrs.PlateCarree(), facecolor='C8', edgecolor='0.5')
-    # # fg.add_geometries([polyNL], crs=ccrs.PlateCarree(), facecolor='C9', edgecolor='0.5')
-    # # fg.add_geometries([polyPL], crs=ccrs.PlateCarree(), facecolor='C1', edgecolor='0.5')
-    # # fg.add_geometries([polyPT], crs=ccrs.PlateCarree(), facecolor='C2', edgecolor='0.5')
-    # # fg.add_geometries([polyRO], crs=ccrs.PlateCarree(), facecolor='C3', edgecolor='0.5')
-    # # fg.add_geometries([polySK], crs=ccrs.PlateCarree(), facecolor='C4', edgecolor='0.5')
-    # # fg.add_geometries([polySI], crs=ccrs.PlateCarree(), facecolor='C5', edgecolor='0.5')
-    # # fg.add_geometries([polyES], crs=ccrs.PlateCarree(), facecolor='C0', edgecolor='0.5')
-    # # fg.add_geometries([polySE], crs=ccrs.PlateCarree(), facecolor='C7', edgecolor='0.5')
-    # # fg.add_geometries([polyUK], crs=ccrs.PlateCarree(), facecolor='C8', edgecolor='0.5')
-    # # fg.add_geometries([polyPE], crs=ccrs.PlateCarree(), facecolor='C9', edgecolor='0.5')
-
     # node name
     for nd in mTEPES.nd:
         # plt.annotate(nd, [mTEPES.pNodeLon[nd], mTEPES.pNodeLat[nd]])
-        # Add text 25 pixels to the left of the volcano.
+        # Add text 25 pixels to the left of each node.
         ax.text(mTEPES.pNodeLon[nd], mTEPES.pNodeLat[nd], nd, fontsize=8,
                 verticalalignment='center', horizontalalignment='right',
                 transform=text_transform,
@@ -454,6 +382,9 @@ def OutputResults(CaseName, mTEPES):
         plt.annotate(round(mTEPES.pLineNTC[ni,nf,cc]*1e3), [(mTEPES.pNodeLon[ni]+mTEPES.pNodeLon[nf])/2, (mTEPES.pNodeLat[ni]+mTEPES.pNodeLat[nf])/2])
 
     plt.title(CaseName+' Network Map')
+
+    # # Place a legend to the right of this smaller subplot.
+    # plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
     # plt.show()
     plt.savefig(CaseName+'/oT_Plot_MapNetwork_'+CaseName+'.png', bbox_inches=None, dpi=1200)
 
