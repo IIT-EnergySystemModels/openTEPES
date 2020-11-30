0ad24ad4f67b01a40741e92f2b303ced54c1d5c7
Erik <erikfilias@gmail.com>
2020-11-30 18:44:25 +0100

    Delete Results in case studies

--- src/test_run.py
+++ src/test_run.py
@@ -13,7 +13,7 @@ StartTime = time.time()
 CaseName = '9n'
 
 #%% model declaration
-mTEPES = ConcreteModel('Open Generation and Transmission Operation and Expansion Planning Model with RES and ESS (openTEPES) - Version 1.7.15 - September 4, 2020')
+mTEPES = ConcreteModel()
 
 # InputData(CaseName, mTEPES)
 
