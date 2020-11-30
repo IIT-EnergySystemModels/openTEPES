e215e5e9809efb73677c22c5db5bddfa8aedfde1
Erik <erikfilias@gmail.com>
2020-11-30 18:52:24 +0100

    Update __init__.py

--- src/openTEPES/__init__.py
+++ src/openTEPES/__init__.py
@@ -4,8 +4,8 @@ __version__ = '0.1.0'
 __author__ = 'Erik Alvarez <erikfilias@gmail.com>'
 __all__ = []
 
-# from .openTEPES import run_openTEPES
-from .openTEPES_InputData import InputData
-from .openTEPES_ModelFormulation import ModelFormulation
-from .openTEPES_ProblemSolving import ProblemSolving
-from .openTEPES_OutputResults import OutputResults
+
+from .openTEPES_InputData              import InputData
+from .openTEPES_ModelFormulation       import ModelFormulation
+from .openTEPES_OutputResults          import OutputResults
+from .openTEPES_ProblemSolving         import ProblemSolving
