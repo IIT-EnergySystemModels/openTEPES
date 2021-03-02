3df0221d375420c33fc360e453e759deb8d23f41
Erik Alvarez <erikfilias@gmail.com>
2021-03-02 16:22:56 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup
 
 
-def local_scheme(version):
-    return ""
+# def local_scheme(version):
+#     return ""
+#
+#
+# setup(use_scm_version={"local_scheme": local_scheme})
 
-
-setup(use_scm_version={"local_scheme": local_scheme})
+setup(use_scm_version=True)
