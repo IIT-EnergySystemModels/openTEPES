3bd9ddbc42c730f697be141effd88d4eaaddf992
Erik Alvarez <erikfilias@gmail.com>
2021-03-02 21:23:41 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,9 +21,9 @@ install_requires = [
 
 setup(
     name="openTEPES",
-    version="2.0.9",
+    version="2.0.10",
     description="Open Generation and Transmission Operation and Expansion Planning Model with RES and ESS (openTEPES)",
-    scripts=glob.glob("scripts/openTEPES*.py"),
+    scripts=["scripts/openTEPES*.py"],
     author_email="andres.ramos@comillas.edu",
     url="https://github.com/IIT-EnergySystemModels/openTEPES",
     install_requires=install_requires,
