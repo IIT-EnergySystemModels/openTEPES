b0040e360197daa74dc596a1aa542e92d73e2c49
Erik Alvarez <erikfilias@gmail.com>
2021-03-03 20:09:38 +0100

    Update __init__.py

--- openTEPES/__init__.py
+++ openTEPES/__init__.py
@@ -4,7 +4,7 @@ __version__ = '0.1.0'
 __author__ = 'Erik Alvarez <ealvarezq@comillas.edu>'
 __all__ = []
 
-
+from .openTEPES import *
 from .openTEPES_InputData import *
 from .openTEPES_ModelFormulation import *
 from .openTEPES_OutputResults import *
