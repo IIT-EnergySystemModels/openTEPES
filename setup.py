07567dc746d94a306b5f413040a7986db23cc570
Erik Alvarez <erikfilias@gmail.com>
2021-03-03 11:48:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-from setuptools import setup
-import glob
+from setuptools import setup, find_packages
 #
 # def local_scheme(version):
 #     return ""
@@ -8,24 +7,22 @@ import glob
 # setup(use_scm_version={"local_scheme": local_scheme})
 
 install_requires = [
-    "PyYAML>=5.3.1",
-    "colorama>=0.4.4",
-    "gym>=0.17.1",
     "matplotlib>=3.2.1",
     "numpy>=1.18.2",
     "pandas>=1.0.3",
     "pyomo",
     "psutil",
-    "glpk",
 ]
 
 setup(
     name="openTEPES",
-    version="2.0.10",
+    version="2.0.12",
     description="Open Generation and Transmission Operation and Expansion Planning Model with RES and ESS (openTEPES)",
     scripts=["scripts/openTEPES.py"],
     author_email="andres.ramos@comillas.edu",
     url="https://github.com/IIT-EnergySystemModels/openTEPES",
     install_requires=install_requires,
-    packages=["openTEPES"],
+    extras_require={
+        'interactive': ['glpk', 'cartopy'], },
+    packages=find_packages(["openTEPES"]),
 )
