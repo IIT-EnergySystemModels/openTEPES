fe361b7651b0e15b35422bd92d864436a08eac78
Erik Alvarez <erikfilias@gmail.com>
2021-03-04 11:15:52 +0100

    Update __init__.py

--- openTEPES/__init__.py
+++ openTEPES/__init__.py
@@ -1,6 +1,16 @@
-"""openTEPES - Transmission expansion planning model
+"""Open Generation and Transmission Operation and Expansion Planning Model with RES and ESS.
 
+    Args:
+        case: Name of the folder where the CSV files of the case are found
+        dir: Main path where the case folder can be found
+        solver: Name of the solver
 
+    Returns:
+        Output results in CSV files that are found in the case folder.
+
+    Examples:
+        >>> import openTEPES as oT
+        >>> oT.routine("9n", "C:\\Users\\UserName\\Documents\\GitHub\\openTEPES", "glpk")
 """
 
 __version__ = '0.1.0'
