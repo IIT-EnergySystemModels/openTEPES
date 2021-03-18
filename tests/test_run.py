d3f08d2e4d92071f8ab57d5d2e9511701fed4b3a
Erik Alvarez <erikfilias@gmail.com>
2021-03-18 21:57:09 +0100

    Update and fix

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,26 +1,19 @@
 # import argparse
 import os
+from openTEPES.openTEPES import openTEPES_run
+import openTEPES
 
 CWD = os.getcwd()
 TEST_PATH = CWD + '/openTEPES'
 os.chdir(TEST_PATH)
-
-print(os.path.abspath(os.path.join(CWD, os.pardir)))
-new_path = os.path.abspath(os.path.join(CWD, os.pardir))
-dir_path = new_path + '/openTEPES/openTEPES'
-os.chdir(dir_path)
-
-from openTEPES.openTEPES import openTEPES_run
-import openTEPES
-
 
 CASE = "9n"
 # parser = argparse.ArgumentParser(description='Introducing main parameters.')
 # parser.add_argument('--case', type=str, default=None)
 # parser.add_argument('--dir', type=str, default=None)
 # parser.add_argument('--solver', type=str, default=None)
-# DIR = os.path.dirname(openTEPES.__file__)
-DIR = dir_path
+DIR = os.path.dirname(openTEPES.__file__)
+# DIR = TEST_PATH
 SOLVER = "glpk"
 
 openTEPES_run(DIR, CASE, SOLVER)
