9352a77c7eae60f965169ca0cd1f7574d26f19ce
Erik Alvarez <erikfilias@gmail.com>
2022-01-19 16:33:30 +0100

    Update openTEPES_Main.py

--- openTEPES/openTEPES_Main.py
+++ openTEPES/openTEPES_Main.py
@@ -65,7 +65,7 @@
 # make it effectively proprietary.  To prevent this, the GPL assures that
 # patents cannot be used to render the program non-free.
 
-# Open Generation and Transmission Operation and Expansion Planning Model with RES and ESS (openTEPES) - Version 4.3.0 - December 31, 2021
+# Open Generation and Transmission Operation and Expansion Planning Model with RES and ESS (openTEPES) - Version 4.3.1 - January 19, 2021
 # simplicity and transparency in power systems planning
 
 # Developed by
