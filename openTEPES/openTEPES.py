446bfd6eeb65925362e61bec453bc1e42939c682
Andres Ramos <aramos@comillas.edu>
2023-01-31 17:07:34 +0100

    update version date

--- openTEPES/openTEPES.py
+++ openTEPES/openTEPES.py
@@ -1,5 +1,5 @@
 """
-Open Generation, Storage, and Transmission Operation and Expansion Planning Model with RES and ESS (openTEPES) - January 30, 2023
+Open Generation, Storage, and Transmission Operation and Expansion Planning Model with RES and ESS (openTEPES) - January 31, 2023
 """
 
 import time
@@ -35,10 +35,10 @@ def openTEPES_run(DirName, CaseName, SolverName, pCompleteResults, pIndLogConsol
     idxDict['y'  ] = 1
 
     #%% model declaration
-    mTEPES = ConcreteModel('Open Generation, Storage, and Transmission Operation and Expansion Planning Model with RES and ESS (openTEPES) - Version 4.10.0 - January 30, 2023')
+    mTEPES = ConcreteModel('Open Generation, Storage, and Transmission Operation and Expansion Planning Model with RES and ESS (openTEPES) - Version 4.10.0 - January 31, 2023')
 
-    pCompleteResults = [j for i, j in idxDict.items() if i == pCompleteResults][0]
-    pIndLogConsole   = [j for i, j in idxDict.items() if i == pIndLogConsole  ][0]
+    pCompleteResults = [j for i,j in idxDict.items() if i == pCompleteResults][0]
+    pIndLogConsole   = [j for i,j in idxDict.items() if i == pIndLogConsole  ][0]
 
     # Define sets and parameters
     InputData(DirName, CaseName, mTEPES, pIndLogConsole)
