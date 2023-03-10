954432fc5a73a6c712453325437f8b9890938918
Erik Alvarez <erikfilias@gmail.com>
2023-03-10 20:30:24 +0100

    Update __init__.py

--- openTEPES/__init__.py
+++ openTEPES/__init__.py
@@ -14,7 +14,7 @@ Open Generation, Storage, and Transmission Operation and Expansion Planning Mode
         >>> import openTEPES as oT
         >>> oT.routine("9n", "C:\\Users\\UserName\\Documents\\GitHub\\openTEPES", "glpk")
 """
-__version__ = "4.10.2"
+__version__ = "4.10.3"
 
 from .openTEPES_Main             import main
 from .openTEPES                  import *
