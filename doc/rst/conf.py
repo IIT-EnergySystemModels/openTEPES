2f41188d115011d62816bd72e8a8ff773450b3be
Erik Alvarez <erikfilias@gmail.com>
2023-05-09 11:06:39 +0200

    update all

--- doc/rst/conf.py
+++ doc/rst/conf.py
@@ -3,9 +3,6 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import sys
-import os
-import shlex
 
 
 # -- Project information -----------------------------------------------------
@@ -28,11 +25,10 @@ release = ''
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-# extensions = [
-#     'sphinx.ext.todo',
-#     'sphinx.ext.githubpages',
-# ]
-extensions = ['sphinx.ext.doctest']
+extensions = [
+    'sphinx.ext.todo',
+    'sphinx.ext.githubpages',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -56,8 +52,7 @@ language = 'en'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-# exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
