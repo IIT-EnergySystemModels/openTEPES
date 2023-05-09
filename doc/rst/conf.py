1b0a8ed96162ea285f31624f862f1398bacb9cf6
Erik Alvarez <erikfilias@gmail.com>
2023-05-09 10:22:44 +0200

    Update conf.py

--- doc/rst/conf.py
+++ doc/rst/conf.py
@@ -25,10 +25,10 @@ release = ''
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.todo',
-    'sphinx.ext.githubpages',
-]
+# extensions = [
+#     'sphinx.ext.todo',
+#     'sphinx.ext.githubpages',
+# ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
