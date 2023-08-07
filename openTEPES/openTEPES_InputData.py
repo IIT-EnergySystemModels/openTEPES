f7a6c18ff75a18daaf9b8498133d0f40b75c4fbf
EladioJES <elajimsar@gmail.com>
2023-08-07 10:04:47 +0200

    Solved issue transforming data that threw a TypeError.

--- openTEPES/openTEPES_InputData.py
+++ openTEPES/openTEPES_InputData.py
@@ -576,7 +576,7 @@ def InputData(DirName, CaseName, mTEPES, pIndLogConsole):
     mTEPES.pLineType = Set(initialize=pLineType.index, ordered=False, doc='line type')
 
     #%% inverse index load level to stage
-    pStageToLevel = pLevelToStage.reset_index().set_index('Stage').set_axis(['LoadLevel'], axis=1, copy=False)[['LoadLevel']]
+    pStageToLevel = pLevelToStage.reset_index().set_index('Stage').set_axis(['LoadLevel'], axis=1)[['LoadLevel']]
     pStageToLevel = pStageToLevel.loc[pStageToLevel['LoadLevel'].isin(mTEPES.n)]
     pStage2Level  = pStageToLevel.reset_index().set_index(['Stage','LoadLevel'])
 
@@ -592,7 +592,7 @@ def InputData(DirName, CaseName, mTEPES, pIndLogConsole):
         mTEPES.pLoadLevelWeight[n] = pStageWeight[st]
 
     #%% inverse index node to generator
-    pNodeToGen = pGenToNode.reset_index().set_index('Node').set_axis(['Generator'], axis=1, copy=False)[['Generator']]
+    pNodeToGen = pGenToNode.reset_index().set_index('Node').set_axis(['Generator'], axis=1)[['Generator']]
     pNodeToGen = pNodeToGen.loc[pNodeToGen['Generator'].isin(mTEPES.g)]
     pNode2Gen  = pNodeToGen.reset_index().set_index(['Node', 'Generator'])
 
@@ -607,7 +607,7 @@ def InputData(DirName, CaseName, mTEPES, pIndLogConsole):
     mTEPES.r2g = Set(initialize=mTEPES.rg*mTEPES.g, ordered=False, doc='region to generator', filter=lambda mTEPES,rg,g: (rg,g) in pRegion2Gen)
 
     #%% inverse index generator to technology
-    pTechnologyToGen = pGenToTechnology.reset_index().set_index('Technology').set_axis(['Generator'], axis=1, copy=False)[['Generator']]
+    pTechnologyToGen = pGenToTechnology.reset_index().set_index('Technology').set_axis(['Generator'], axis=1)[['Generator']]
     pTechnologyToGen = pTechnologyToGen.loc[pTechnologyToGen['Generator'].isin(mTEPES.g)]
     pTechnology2Gen  = pTechnologyToGen.reset_index().set_index(['Technology', 'Generator'])
 
@@ -625,7 +625,7 @@ def InputData(DirName, CaseName, mTEPES, pIndLogConsole):
     mTEPES.psnrt = [(p,sc,n,rt) for p,sc,n,rt in mTEPES.psn*mTEPES.rt]
 
     #%% inverse index generator to mutually exclusive generator
-    pExclusiveGenToGen = pGenToExclusiveGen.reset_index().set_index('MutuallyExclusive').set_axis(['Generator'], axis=1, copy=False)[['Generator']]
+    pExclusiveGenToGen = pGenToExclusiveGen.reset_index().set_index('MutuallyExclusive').set_axis(['Generator'], axis=1)[['Generator']]
     pExclusiveGenToGen = pExclusiveGenToGen.loc[pExclusiveGenToGen['Generator'].isin(mTEPES.g)]
     pExclusiveGen2Gen  = pExclusiveGenToGen.reset_index().set_index(['MutuallyExclusive', 'Generator'])
 
