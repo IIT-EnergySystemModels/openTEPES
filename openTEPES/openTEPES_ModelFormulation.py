e8968732643bfe56f7168bf715a199ed716e3968
Andres Ramos <61053256+arght@users.noreply.github.com>
2024-02-09 18:41:45 +0100

    change date

--- openTEPES/openTEPES_ModelFormulation.py
+++ openTEPES/openTEPES_ModelFormulation.py
@@ -1,5 +1,5 @@
 """
-Open Generation, Storage, and Transmission Operation and Expansion Planning Model with RES and ESS (openTEPES) - February 08, 2024
+Open Generation, Storage, and Transmission Operation and Expansion Planning Model with RES and ESS (openTEPES) - February 09, 2024
 """
 
 import time
