1f82fdba75b6e59da2589dbbfb9534578ecdfaea
Erik Alvarez <erikfilias@gmail.com>
2024-09-19 11:03:24 +0200

    Update test_run.py

--- tests/test_run.py
+++ tests/test_run.py
@@ -42,4 +42,4 @@ def case_9n_7d_system():
 
 def test_openTEPES_run(case_9n_7d_system):
     mTEPES = openTEPES_run(**case_9n_7d_system)
-    np.testing.assert_approx_equal(pyo.value(mTEPES.eTotalSCost), 6.163409195706696)
+    np.testing.assert_approx_equal(pyo.value(mTEPES.eTotalSCost), 5.5757755587859075)
