fb26e07ea1fcc5b544d6d48f5ed50a622c3cd06a
Erik Alvarez <erikfilias@gmail.com>
2024-09-20 10:14:20 +0200

    Update test_run.py

--- tests/test_run.py
+++ tests/test_run.py
@@ -25,8 +25,12 @@ def case_9n_7d_system():
     RESEnergy_csv = os.path.join(
         data["DirName"], data["CaseName"], f"oT_Data_RESEnergy_{data['CaseName']}.csv"
     )
+    stage_csv = os.path.join(
+        data["DirName"], data["CaseName"], f"oT_Data_Stage_{data['CaseName']}.csv"
+    )
     original_duration_df = pd.read_csv(duration_csv, index_col=[0, 1, 2])
     original_resenergy_df = pd.read_csv(RESEnergy_csv, index_col=[0, 1])
+    original_stage_df = pd.read_csv(stage_csv, index_col=[0])
     try:
         df = original_duration_df.copy()
         df.iloc[170:, df.columns.get_loc("Duration")] = np.nan
@@ -35,13 +39,19 @@ def case_9n_7d_system():
         df = original_resenergy_df.copy()
         df.iloc[0:, df.columns.get_loc("RESEnergy")] = np.nan
         df.to_csv(RESEnergy_csv)
+
+        df = original_stage_df.copy()
+        df.iloc[0:, df.columns.get_loc("Weight")] = 52
+        df.to_csv(stage_csv)
+
         yield data
     finally:
         original_duration_df.to_csv(duration_csv)
         original_resenergy_df.to_csv(RESEnergy_csv)
+        original_stage_df.to_csv(stage_csv)
 
 
 def test_openTEPES_run(case_9n_7d_system):
     mTEPES = openTEPES_run(**case_9n_7d_system)
     assert mTEPES is not None
-    # np.testing.assert_approx_equal(pyo.value(mTEPES.eTotalSCost), 6.2137322775924)
+    np.testing.assert_approx_equal(pyo.value(mTEPES.eTotalSCost), 250.6271831191815)
