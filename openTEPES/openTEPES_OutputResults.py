d245a99b4ef75e1f090e5d2a9275122a3cc0b654
Andres Ramos <61053256+arght@users.noreply.github.com>
2024-10-20 21:35:34 +0200

    update dates

--- openTEPES/openTEPES_OutputResults.py
+++ openTEPES/openTEPES_OutputResults.py
@@ -1,5 +1,5 @@
 """
-Open Generation, Storage, and Transmission Operation and Expansion Planning Model with RES and ESS (openTEPES) - October 17, 2024
+Open Generation, Storage, and Transmission Operation and Expansion Planning Model with RES and ESS (openTEPES) - October 20, 2024
 """
 
 import time
