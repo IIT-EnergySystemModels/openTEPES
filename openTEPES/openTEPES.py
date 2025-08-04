c4a7b5e8927010f7e157cc9493fcc7bf4eae3707
Andres Ramos <aramos@comillas.edu>
2025-08-04 09:38:36 +0200

    Update openTEPES.py

--- openTEPES/openTEPES.py
+++ openTEPES/openTEPES.py
@@ -1,5 +1,5 @@
 """
-Open Generation, Storage, and Transmission Operation and Expansion Planning Model with RES and ESS (openTEPES) - July 22, 2025
+Open Generation, Storage, and Transmission Operation and Expansion Planning Model with RES and ESS (openTEPES) - August 04, 2025
 """
 
 # import dill as pickle
