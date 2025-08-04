3cccb5d6525bbac85478c0b372ebbb30db37bc08
Erik Alvarez <erikfilias@gmail.com>
2025-08-04 14:13:00 +0200

    Update test_run.py

--- tests/test_run.py
+++ tests/test_run.py
@@ -69,7 +69,7 @@ def case_7d_system(request):
 # === Parametrized Test ===
 @pytest.mark.parametrize("case_7d_system,expected_cost", [
     ("9n", 248.43328653794),
-    ("sSEP", 38623.89741870424),
+    ("sSEP", 38581.3355242721),
 ], indirect=["case_7d_system"])
 def test_openTEPES_run(case_7d_system, expected_cost):
     """
